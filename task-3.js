5c38baa70794b12c08e9ba19e4b738e461f66e25
ROKSANA DILSHAD <roksanadilshad@gmail.com>
2025-08-08 20:39:30 +0600

    task-3.js

---
+++ task-3.js
@@ -0,0 +1,9 @@
+function make_avg(numbers, size){
+    let sum = 0;
+    for(const number of numbers){
+        sum += number;  //age plus trpor equal
+    }
+    return sum / size;
+}
+const result = [10, 26, 27, 15,3];
+console.log(make_avg(result, result.length)); //upore joyta perameter ase tototae dite hbe ekhane budddhu
